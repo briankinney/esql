3a51d55b42b2aa814bca62a10967adcf7c2820b7
Brian Kinney <briankanokinney@gmail.com>
2017-10-20 00:17:45 -0400

    Use API differently

--- src/main/java/com/briankinney/esql/cli/Tool.java
+++ src/main/java/com/briankinney/esql/cli/Tool.java
@@ -4,17 +4,27 @@ import com.briankinney.esql.QueryBuilderListener;
 import com.briankinney.esql.esqlLexer;
 import com.briankinney.esql.esqlParser;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class Tool {
     public static void main(String[] args) {
         InputStream inputStream = System.in;
-        CharStream charStream = new UnbufferedCharStream(inputStream);
+        CharStream charStream = null;
+        try {
+            charStream = CharStreams.fromStream(inputStream);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
         TokenSource tokenSource = new esqlLexer(charStream);
         TokenStream tokenStream = new CommonTokenStream(tokenSource);
         esqlParser parser = new esqlParser(tokenStream);
-        parser.addParseListener(new QueryBuilderListener());
-        parser.search_query();
+        esqlParser.Search_queryContext searchQuery = parser.search_query();
+
+        ParseTreeWalker.DEFAULT.walk(new QueryBuilderListener(), searchQuery);
     }
 }
